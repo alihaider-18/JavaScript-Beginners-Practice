3c89b8776b6724012e7825ec39a23c7f7f5faf9c
alihaider-18 <ah18contact@gmail.com>
2024-02-21 16:41:22 +0530

    updated 06.js

--- 06.js
+++ 06.js
@@ -1,7 +1,15 @@
 // **Count Vowels in a String**: Write a JavaScript function that counts the number
 // of vowels in a given string.
 
-function calVowels(str){
-    let lowercase = str.toLowerCase();
-    
+function countVowels(str){
+    let lowerSrt = str.toLowerCase();
+    let vowelsCount = 0;
+    for ( let char of lowerSrt){
+      if ('aeiou'.includes(char))
+      vowelsCount++; 
+    }
+    return vowelsCount;
 }
+
+str = 'AliHaider';
+console.log(countVowels(str));
