daa950d84ad0b5b6085d64f6c20b04e62dc1888c
alihaider-18 <ah18contact@gmail.com>
2024-02-26 16:45:40 +0530

    done ques 7

--- 07.js
+++ 07.js
@@ -1,2 +1,16 @@
 // Check for Prime Number: Write a JavaScript function that checks whether a given 
 // number is prime or not.
+
+function isPrime(num){
+    if (num <= 1){
+        return false;
+    }
+
+    for (let i = 2; i < Math.sqrt(num); i++){
+        if (num % i === 0)
+        return false;
+    }
+    return true;
+}
+
+console.log(isPrime(7));
