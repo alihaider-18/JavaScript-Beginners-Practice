f0f8a1877a2053b9d53642dec8dfe171f40fc4e9
alihaider-18 <ah18contact@gmail.com>
2024-03-17 14:31:55 +0530

    Solved

--- 08.js
+++ 08.js
@@ -1,2 +1,23 @@
 // Find the Longest Word in a String: Write a JavaScript function that takes a
 // sentence as input and returns the longest word in the sentence.
+
+function findLongestWord(str){
+    const words = str.split(' ');
+
+    let longestWord = "";
+
+    for (let i = 0; i < words.length; i++){
+        const currentWord = words[i];
+
+        if(currentWord.length > longestWord.length){
+            longestWord = currentWord;
+        }
+    }
+
+    return longestWord;
+}
+
+const str = "this world is an very interesting place";
+const longest = findLongestWord(str);
+console.log(`Longest Word : ${longest}`);
+
